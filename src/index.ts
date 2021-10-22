339225c17308dc4ecef8e60487a110f812d4fd13
Nox <alexanderisasimp4anime@gmail.com>
2021-10-22 10:24:53 +0545

    Fix #2
    Everything is breaking down qwq

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
-import DiscordJS, { Intents } from 'discord.js';
-import WOKCommands from 'wokcommands';
-import path from 'path';
-import dotenv from 'dotenv';
+import DiscordJS, { Intents } from "discord.js";
+import WOKCommands from "wokcommands";
+import path from "path";
+import dotenv from "dotenv";
 dotenv.config();
 
 const client = new DiscordJS.Client({
-	intents: [
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MESSAGES,
-	],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user?.tag}!`);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
 
-	new WOKCommands(client, {
-		 commandsDir: path.join(__dirname, '../commands'),
-		 featuresDir: path.join(__dirname, '../features'),
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, "../commands"),
+    featuresDir: path.join(__dirname, "../features"),
 
-		 typeScript: true,
-	})
+    typeScript: true,
+  });
 });
 
 client.login(process.env.TOKEN);

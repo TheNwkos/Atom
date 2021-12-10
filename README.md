e4e07d1687ca20733e1d101a0deeff4d099a1c79
Nox <83485955+Nwko@users.noreply.github.com>
2021-12-10 12:26:20 +0545

    Fix #5

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Atom
+
 Source Code For Atom#1214
+
+# Requirements
+
+- [Node](https://nodejs.org/en) v.16.6.0 or later LTS recommended
+- [Git](https://git-scm.com/downloads) v.2.12.0 or later
+- [npm](https://www.npmjs.com/) v.7 or later or [yarn](https://yarnpkg.com/) v.1.3 or later or [pnpm](https://pnpm.js.org/) v.5 or later
+
+# Building
+
+    npm run build
+    or
+    git pull && npm install
+
+# Running
+
+    npm run dev
+    or
+    ts-node src/index.ts
+
+# Documentation
+
+If your looking for the documentation to further understand the code, please refer to the [Discord.JS](https://discord.js.org/#/) website and for the command handler refer to [WOKCommands](https://docs.wornoffkeys.com/).
--- commands/RandomFacts.ts
+++ commands/RandomFacts.ts
@@ -1,12 +1,29 @@
-import { ICommand } from 'wokcommands'
+import { ICommand } from "wokcommands";
+import { facts } from "../facts.json";
 
 export default {
-    category: 'Test',
-    description: 'Test',
+  category: "Test",
+  description: "Test",
 
-    callback: ({}) => {
-        var facts = ['The DNA sequence of Stephen Hawking is stored on a hard disk, called the “Immortality Drive,” onboard the ISS. He is among a select group of humans to have their DNA immortalized.', 'fact', 'fact2'],
+  callback: ({ message, interaction }) => {
+    // var facts = [
+    //   "The DNA sequence of Stephen Hawking is stored on a hard disk, called the “Immortality Drive,” onboard the ISS. He is among a select group of humans to have their DNA immortalized.",
+    //   "fact",
+    //   "fact2",
+    // ];
 
-        var randomFacts = facts[Math.floor(Math.random() * facts.length)]
+    var facts = facts[Math.floor(Math.random() * facts.length)];
+
+    if (message) {
+      message.reply({
+        content: facts,
+      });
     }
+
+    if (interaction) {
+      interaction.reply({
+        content: facts,
+      });
+    }
+  },
 } as ICommand;
